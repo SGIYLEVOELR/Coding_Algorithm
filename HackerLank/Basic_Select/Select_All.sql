9c0c755040e0ec0e50f5e9f66e019f4655e92340
은결 <80661496+Eungyeol41@users.noreply.github.com>
2022-07-11 14:49:04 +0900

    Update Select_All.sql

--- HackerLank/Basic_Select/Select_All.sql
+++ HackerLank/Basic_Select/Select_All.sql
@@ -4,7 +4,7 @@
 			
 			CITY
 	Field			Type
-	ID				NUMBER
+	ID			NUMBER
 	NAME			VARCHAR2(17)
 	COUNTRYCODE		VARCHAR2(3)
 	DISTRICT		VARCHAR2(20)
@@ -15,13 +15,13 @@ SELECT * FROM CITY;
 
 -- Expected Output
 
--- ID	 	NAME			COUNTRYCODE		DISTRICT			POPULATION
--- 6	 	Rotterdam		NLD		 		Zuid-Holland 		593321 
--- 3878	 	Scottsdale		USA		 		Arizona 			202705 
--- 3965	 	Corona			USA		 		California 			124966 
--- 3973	 	Concord			USA		 		California 			121780 
--- 3977	 	Cedar			Rapids	 		USA Iowa 			120758 
+-- ID	 	NAME			COUNTRYCODE		DISTRICT		POPULATION
+-- 6	 	Rotterdam		NLD		 	Zuid-Holland 		593321 
+-- 3878	 	Scottsdale		USA		 	Arizona 		202705 
+-- 3965	 	Corona			USA		 	California 		124966 
+-- 3973	 	Concord			USA		 	California 		121780 
+-- 3977	 	Cedar			Rapids	 		USA Iowa 		120758 
 -- 3982	 	Coral			Springs	 		USA Florida 		117549 
--- 4054	 	Fairfield		USA		 		California 			92256 
--- 4058	 	Boulder			USA		 		Colorado 			91238 
+-- 4054	 	Fairfield		USA		 	California 		92256 
+-- 4058	 	Boulder			USA		 	Colorado 		91238 
 -- 4061	 	Fall			River	 		USA Massachusetts 	90555 
