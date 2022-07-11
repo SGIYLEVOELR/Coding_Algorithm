6d000cb6b018b1c11c46f17f23e86a6cf743f8c3
Eungyeol41 <iniziato@naver.com>
2022-07-11 15:22:35 +0900

    sql문 수정

--- HackerLank/Basic_Select/Weather_Observation_Station_3.sql
+++ HackerLank/Basic_Select/Weather_Observation_Station_3.sql
@@ -3,13 +3,13 @@
 	Print the results in any order, but exclude duplicates from the answer.
 	The STATION table is described as follows:
 			
-		STATION
-	Field		Type
-	ID			NUMBER
-	CITY		VARCHAR2(21)
-	STATE		VARCHAR2(2)
-	LAT_N		NUMBER
-	LONG_W		NUMBER
+			STATION
+		Field		Type
+		ID			NUMBER
+		CITY		VARCHAR2(21)
+		STATE		VARCHAR2(2)
+		LAT_N		NUMBER
+		LONG_W		NUMBER
 
 	where LAT_N is the northern latitude and LONG_W is the western longitude.
 
